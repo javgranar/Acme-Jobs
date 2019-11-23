5598dac37579ab461ee5e63e49c1423f4f8aa985
javgranar <48645270+javgranar@users.noreply.github.com>
2019-11-23 20:11:11 +0100

    Corrección
    He eliminado esta linea Offer existing; , debido a que no se usa

--- src/main/java/acme/features/authenticated/offer/AuthenticatedOfferCreateService.java
+++ src/main/java/acme/features/authenticated/offer/AuthenticatedOfferCreateService.java
@@ -93,7 +93,6 @@ public class AuthenticatedOfferCreateService implements AbstractCreateService<Au
 
 		Calendar calendar;
 		Date minimumDeadline;
-		Offer existing;
 
 		if (!errors.hasErrors("deadline")) {
 			calendar = new GregorianCalendar();
